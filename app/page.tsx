aaaac294c1de12a1692d0617519fe892dc499006
Xen14581 <72184281+Xen14581@users.noreply.github.com>
2023-10-02 16:27:01 +0530

    Sidebar finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,41 @@
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { TypographyLarge } from "@/components/ui/typographylarge";
+import { TypographyMuted } from "@/components/ui/typographymuted";
 import { ProjectCard } from "../components/projectcard";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { Settings2 } from "lucide-react";
 
 export default function Home() {
   return (
-    <main className="min-h-screen grid grid-cols-5">
+    <main className="min-h-screen grid grid-cols-5 overflow-hidden">
       {/* Projects Sidebar */}
-      <div className="bg-slate-900 w-full h-full col-span-1 p-4 flex flex-col">
+      <div className="bg-slate-900 w-full h-screen col-span-1 p-4 flex flex-col">
         <Button variant="outline" className="w-full">
           New Project
         </Button>
-        <div className="flex-auto py-3">
+        <div className="py-3 h-[85%] mb-4">
           <TypographyLarge>Projects</TypographyLarge>
-          <div className="grid grid-cols-1 gap-0 py-2">
-            <ScrollArea className="h-screen">
-              {[1, 2, 3, 4, 5].map((el) => (
+
+          <ScrollArea className="h-full">
+            <div className="grid grid-cols-1 gap-0 py-2">
+              {[
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18,
+                19, 20,
+              ].map((el) => (
                 <ProjectCard key={el} title={`Project ${el}`} />
               ))}
-            </ScrollArea>
+            </div>
+          </ScrollArea>
+        </div>
+        <div className="flex flex-auto justify-between place-items-center">
+          <div className="pt-2">
+            <TypographyMuted>Logged in as:</TypographyMuted>
+            <TypographyLarge>Anonymous</TypographyLarge>
           </div>
+          <Button variant="ghost">
+            <Settings2 />
+          </Button>
         </div>
       </div>
 
--- components/projectcard.jsx
+++ components/projectcard.jsx
@@ -3,7 +3,7 @@ import { MessagesSquare } from "lucide-react";
 
 export function ProjectCard({ title }) {
   return (
-    <Button variant="ghost" className="outline-none flex justify-start h-1/4 gap-2">
+    <Button variant="ghost" className="outline-none flex justify-start h-1/4 gap-1">
       <div className="col-span-1">
         <MessagesSquare />
       </div>
